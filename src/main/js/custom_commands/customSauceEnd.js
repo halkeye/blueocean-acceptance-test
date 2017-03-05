de26690c0f4eeaf0fbe87e65676c6bf85c2324ac
Gavin Mogan <gavin@saucelabs.com>
2017-03-05 15:59:59 -0800

    don't override job name if its not defined

--- src/main/js/custom_commands/customSauceEnd.js
+++ src/main/js/custom_commands/customSauceEnd.js
@@ -1,20 +1,23 @@
 exports.command = function(cb) {
     var sessionid = this.capabilities['webdriver.remote.sessionid'];
-    console.log(this.currentTest);
+    console.log(this);
     var jobName = this.currentTest.name;
-    console.log("SauceOnDemandSessionID=" + sessionid + " job-name=" + jobName);
+    if (jobName) {
+        console.log("SauceOnDemandSessionID=" + sessionid + " job-name=" + jobName);
+    } else {
+        console.log("SauceOnDemandSessionID=" + sessionid);
+    }
 
     if (process.env.SAUCE_USERNAME) {
-      var SauceLabs = require("saucelabs");
-      var saucelabs = new SauceLabs({
-          username: process.env.SAUCE_USERNAME,
-          password: process.env.SAUCE_ACCESS_KEY
-      });
+        var SauceLabs = require("saucelabs");
+        var saucelabs = new SauceLabs({
+            username: process.env.SAUCE_USERNAME,
+            password: process.env.SAUCE_ACCESS_KEY
+        });
+        var params = { passed: this.currentTest.results.failed === 0 };
+        if (jobName) { params.name = jobName; }
 
-      saucelabs.updateJob(sessionid, {
-          passed: this.currentTest.results.failed === 0,
-          name: jobName
-      }, cb);
+        saucelabs.updateJob(sessionid, params, cb);
     }
 
 };
